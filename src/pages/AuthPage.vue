bacb807bad7e5dfb820a2ca92c9459402980c035
Jader Cerqueira <jader58@live.com>
2026-02-24 13:40:26 -0300

    Testando detalhes dos circulos no background

--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,9 +1,11 @@
 <template>
   <v-container
-    class="d-flex justify-center align-center fill-height"
-    style="background-color: #f2f3fe"
+    class="auth-container d-flex justify-center align-center fill-height"
   >
-    <AuthForm :initialTab="initialTab" @close="handleClose" />
+    <div class="circle-1"></div>
+    <div class="circle-2"></div>
+    <div class="circle-3"></div>
+    <AuthForm :initialTab="initialTab" @close="handleClose" class="auth-form" />
   </v-container>
 </template>
 
@@ -23,5 +25,37 @@ const handleClose = () => {
   router.push('/');
 };
 </script>
+<style scoped>
+.auth-container {
+  background-color: #f2f3fe;
+  position: relative;
+  overflow: hidden;
+}
 
-<style scoped></style>
+.auth-form {
+  position: relative;
+  z-index: 1;
+}
+
+.auth-container > div[class^="circle-"] {
+  position: absolute;
+  border-radius: 50%;
+  background-color: #aaadd5;
+  z-index: 0;
+}
+
+.circle-1 {
+  width: 350px;
+  height: 350px;
+  top: -120px;
+  left: -150px;
+}
+
+.circle-2 {
+  width: 500px;
+  height: 500px;
+  bottom: -200px;
+  right: -200px;
+}
+
+</style>
