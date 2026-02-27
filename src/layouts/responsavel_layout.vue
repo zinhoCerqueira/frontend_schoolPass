5f57c1eaef1300df8b38aeaaec8ffddf771e54db
Jader Cerqueira <jader58@live.com>
2026-02-27 10:15:47 -0300

    Adicionando para responsável um background com blur no layout

--- src/layouts/responsavel_layout.vue
+++ src/layouts/responsavel_layout.vue
@@ -3,7 +3,21 @@
     <RespAppBar />
 
     <v-main class="main-content">
-      <router-view />
+
+      <div class="background-layer">
+        <div class="circle-1">
+          <v-icon class="bg-icon icon-school" icon="mdi-school-outline" />
+        </div>
+
+        <div class="circle-2">
+          <v-icon class="bg-icon icon-car" icon="mdi-car-side" />
+        </div>
+      </div>
+
+      <div class="content-wrapper">
+        <router-view />
+      </div>
+
     </v-main>
 
     <RespBottomNav />
@@ -21,6 +35,59 @@ import RespBottomNav from "@/components/resp/RespBottomNav.vue";
 }
 
 .main-content {
+  position: relative;
   background-color: #f6f6f8;
+  overflow: hidden;
+}
+
+.background-layer {
+  position: absolute;
+  inset: 0;
+  z-index: 0;
+  filter: blur(80px);
+  opacity: 0.6;
+}
+
+.content-wrapper {
+  position: relative;
+  z-index: 1;
+}
+
+.background-layer > div[class^="circle-"] {
+  position: absolute;
+  border-radius: 50%;
+  background-color: #aaadd5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.circle-1 {
+  width: 350px;
+  height: 350px;
+  top: -120px;
+  left: -150px;
+}
+
+.circle-2 {
+  width: 500px;
+  height: 500px;
+  bottom: -200px;
+  right: -200px;
+}
+
+.bg-icon {
+  color: #d1d3ed;
+  pointer-events: none;
+}
+
+.icon-school {
+  font-size: 200px;
+  transform: translate(25%, 25%) rotate(15deg);
+}
+
+.icon-car {
+  font-size: 300px;
+  transform: translate(-25%, -25%) rotate(-10deg);
 }
 </style>
